f208006d33195380d0cccf20c4304b8310d47727
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 22:47:24 +0200

    make five  methods in Player method

---
+++ src/FirstLesson/Task17/Player.java
@@ -0,0 +1,32 @@
+package FirstLesson.Task17;
+public class Player {
+    String colour;
+    int veight;
+    int width;
+    int depth;
+    double volume;
+
+    public void turnPlayer (){
+
+    }
+
+    public void switchOffPlayer (){
+
+    }
+
+    public void nextSoundPlayer (){
+
+    }
+
+    public void changeVolumePlayer (){
+
+    }
+
+    public void playMusicPlayer (){
+
+    }
+
+
+
+
+}

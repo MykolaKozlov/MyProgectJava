5ac34712ee43b6185d3e76efce1ba235b4fe6d96
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 22:40:17 +0200

    make five  methods in NotebookMouse method

---
+++ src/FirstLesson/Task17/NotebookMouse.java
@@ -0,0 +1,29 @@
+package FirstLesson.Task17;
+public class NotebookMouse {
+    String model;
+    String colour;
+    int veight;
+    String connectionType;
+    String sensorType;
+
+    public void plugInMouse (){
+
+    }
+
+    public void pushRightButtononMouse (){
+
+    }
+
+    public boolean mouseIsMoveble (){
+        return true;
+    }
+
+    public void pushLeftButtononMouse (){
+
+    }
+
+    public void pushScrollButtononMouse (){
+
+    }
+
+}

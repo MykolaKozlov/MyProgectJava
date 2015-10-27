47e148d7a84464c64f2fb6ffc7a2d55e652c4356
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 22:33:39 +0200

    make twenty five  methods in Notebook method

---
+++ src/FirstLesson/Task17/Notebook.java
@@ -0,0 +1,129 @@
+package FirstLesson.Task17;
+public class Notebook {
+    String model;
+    String colour;
+    int veight;
+    int width;
+    int depth;
+    double diagonal;
+    int age;
+    int memoryHardDrive;
+    int operationMemory;
+    String memoryType;
+    String processorName;
+    int processorCoreCount;
+    String processorModelNumber;
+    double processorSpeed;
+    boolean bluetooth;
+    String operatingSystem;
+    int warranty;
+    boolean memoryCardSlot;
+    boolean camera;
+    boolean opticalDrive;
+    long workOperationInSeconds;
+    int numberOfUSB;
+    int numberOfHDMI;
+    String otherPortConnections;
+    boolean Speakers;
+
+    public void turnNotebook(){
+
+    }
+
+    public void reloadNotebook(){
+
+    }
+
+    public void updateWindowsNotebook(){
+
+    }
+
+    public void runJavaProgramNotebook (){
+
+    }
+
+    public void disableNotebook (){
+
+    }
+
+    public void turnFilmNotebook (){
+
+    }
+
+    public void turnMusicNotebook (){
+
+    }
+
+    public boolean isEnabledNotebook (){
+        return false;
+    }
+
+    public void turnSkypeNotebook (){
+
+    }
+
+    public void instalProgramNotebook (){
+
+    }
+
+    public void deleteProgramNotebook (){
+
+    }
+
+    public void installWindowsNotebook (){
+
+    }
+
+    public void openBrouserNotebook (){
+
+    }
+
+    public void pluginNewDeviceNotebook (){
+
+    }
+
+    public void createNewFolderNotebook (){
+
+    }
+
+    public void deleteFolderNotebook (){
+
+    }
+
+    public void installDriversNotebook (){
+
+    }
+
+    public void dounloadInformationFromInternetNotebook (){
+
+    }
+
+    public void chengeSettingsNotebook (){
+
+    }
+
+    public void openCalculatorNotebook (){
+
+    }
+
+    public boolean isWorkingOnNotebook (){
+        return true;
+    }
+
+    public void changeProfileNotebook (){
+
+    }
+
+    public void playGameOnNotebook (){
+
+    }
+
+    public void changePasswordNotebook (){
+
+    }
+
+    public void engagedInProgrammingOnNotebook (){
+
+    }
+
+}

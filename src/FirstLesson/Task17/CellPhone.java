ded34acc77cc1f219fa2886f6287a4d03dd48a07
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 22:09:00 +0200

    make five metods

---
+++ src/FirstLesson/Task17/CellPhone.java
@@ -0,0 +1,31 @@
+package FirstLesson.Task17;
+public class CellPhone {
+    String model;
+    String colour;
+    int veight;
+    int width;
+    int depth;
+    double diagonal;
+
+    public String dialTheNumber (String number){
+        return number;
+    }
+
+    public void call (String number){
+
+    }
+
+    public void takeTheChallenge (){
+
+    }
+
+    public boolean isSubscriberIsBusy (){
+        return true;
+    }
+
+    public void turnPhone (){
+
+    }
+
+
+}

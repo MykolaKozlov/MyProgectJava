eed2b7dcb5a39b0468720f2af8c13ee12bab6029
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 22:54:21 +0200

    make five  methods in Table method

---
+++ src/FirstLesson/Task17/Table.java
@@ -0,0 +1,28 @@
+package FirstLesson.Task17;
+public class Table {
+    String colour;
+    int veight;
+    int width;
+    double volume;
+    String model;
+
+    public void assembleTable (){
+
+    }
+
+    public void disassembleTable (){
+
+    }
+
+    public boolean tableIsTree (){
+        return true;
+    }
+
+    public void layTheCloth (){
+
+    }
+
+    public boolean TablesColour (String colour){
+        return false;
+    }
+}

09a7a0867fd2fe54115bb7e69b35a5a4886da59c
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-27 21:36:50 +0200

    change printTankInfo

--- src/FirstLesson/Task14/RunTank.java
+++ src/FirstLesson/Task14/RunTank.java
@@ -3,13 +3,12 @@ public class RunTank {
     public static void main(String[] args) {
         printTankInfo();
     }
-    static void printTankInfo() {
+    public static void printTankInfo() {
         Tank tank = new Tank();
         tank.colour = "blue";
         tank.crew = 10;
-        tank.spead = 60;
-        System.out.println(tank.colour);
-        System.out.println(tank.crew);
-        System.out.println(tank.spead);
+        tank.speed = 60;
+        System.out.println("Tank colour = " + tank.colour + ";" + " Tank crew = " + tank.crew + ";" + " Tank speed = " + tank.speed+ ";");
     }
+
 }

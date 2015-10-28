a541732a29dcbb4c29ddffeaddd7d60cb4971aaa
MykolaKozlov <mykolakozlov@gmail.com>
2015-10-29 00:26:37 +0200

    add enum Color

---
+++ src/FirstLesson/HomeworkFirstLesson/Enum/Color.java
@@ -0,0 +1,4 @@
+package FirstLesson.HomeworkFirstLesson.Enum;
+public enum Color {
+    BLACK, WHITE, BROWN, GREY, YELLOW, BLUE, GREEN, RED, ORANGE, PURPLE, PINK, VIOLET, DARK_BLUE, DARK_RED, DARK_ORANGE, DARK_GREEN;
+}

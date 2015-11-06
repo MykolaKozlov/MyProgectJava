755eeb8f11c3c03dedbe8087d57023292a4388e3
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-06 15:30:35 +0200

    add folder new Tank

---
+++ src/SecondLesson/Chapter_5_Tank_New/Direction.java
@@ -0,0 +1,8 @@
+package SecondLesson.Chapter_5_Tank_New;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT, STOP;
+
+    private Direction(){
+    }
+}
---
+++ src/SecondLesson/Chapter_5_Tank_New/Louncher.java
@@ -0,0 +1,8 @@
+package SecondLesson.Chapter_5_Tank_New;
+
+public class Louncher {
+    public static void main(String[] args) throws Exception {
+        ActionField actionField = new ActionField();
+        actionField.runTheGame();
+    }
+}

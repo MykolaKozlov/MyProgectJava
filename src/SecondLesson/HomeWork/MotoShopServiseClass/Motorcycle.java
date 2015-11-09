7f886a6caaeffa7a80320daa2a2d721011fa0e48
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-09 16:08:05 +0200

    change motorcycle class

--- src/SecondLesson/HomeWork/MotoShopServiseClass/Motorcycle.java
+++ src/SecondLesson/HomeWork/MotoShopServiseClass/Motorcycle.java
@@ -4,13 +4,6 @@ public class Motorcycle {
     private MotorcycleBrand motorcycleBrand;
     private MotorcycleCategory motorcycleCategory;
     private MotorcycleColour motorcycleColour;
-//    private double weight;
-//    private int length;
-//    private String number;
-//    private int engineCapacity;
-//    private int maxSpeed;
-//    private int fuelTank;
-//    private int power;
     private double price;
     private int quantity;
 
@@ -33,34 +26,6 @@ public class Motorcycle {
     public MotorcycleColour getMotorcycleColour() {
         return motorcycleColour;
     }
-
-//    public double getWeight() {
-//        return weight;
-//    }
-//
-//    public int getLength() {
-//        return length;
-//    }
-
-//    public String getNumber() {
-//        return number;
-//    }
-
-//    public int getEngineCapacity() {
-//        return engineCapacity;
-//    }
-//
-//    public int getMaxSpeed() {
-//        return maxSpeed;
-//    }
-//
-//    public int getFuelTank() {
-//        return fuelTank;
-//    }
-//
-//    public int getPower() {
-//        return power;
-//    }
 
     public int getQuantity() {
         return quantity;
@@ -85,32 +50,4 @@ public class Motorcycle {
     public void setMotorcycleColour(MotorcycleColour motorcycleColour) {
         this.motorcycleColour = motorcycleColour;
     }
-
-//    public void setWeight(double weight) {
-//        this.weight = weight;
-//    }
-//
-//    public void setLength(int length) {
-//        this.length = length;
-//    }
-
-//    public void setNumber(String number) {
-//        this.number = number;
-//    }
-
-//    public void setEngineCapacity(int engineCapacity) {
-//        this.engineCapacity = engineCapacity;
-//    }
-//
-//    public void setMaxSpeed(int maxSpeed) {
-//        this.maxSpeed = maxSpeed;
-//    }
-//
-//    public void setFuelTank(int fuelTank) {
-//        this.fuelTank = fuelTank;
-//    }
-//
-//    public void setPower(int power) {
-//        this.power = power;
-//    }
 }

96150c803a11ba06242f2b8ac415938272085f51
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-17 20:02:36 +0200

    change rectangle

--- src/ThirdLesson/Chapter_2_Task_6/Rectangle.java
+++ src/ThirdLesson/Chapter_2_Task_6/Rectangle.java
@@ -12,9 +12,9 @@ public class Rectangle extends Shape {
         Color old = graphics.getColor();
         Color color = new Color(255, 255, 0);
         graphics.setColor(color);
-        graphics.fillRect(300, 25, 100, 100);
+        graphics.fillRect(300, 25, 150, 100);
         Graphics2D graphics2D = (Graphics2D) graphics;
         graphics2D.setStroke(new BasicStroke(10));
-        graphics.drawRect(300, 150, 100, 100);
+        graphics.drawRect(300, 150, 150, 100);
     }
 }

ca2171e8d0cd342982586bcc717322d65240dd53
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-18 15:29:24 +0200

    Tiger draw and destroy interface

---
+++ src/ThirdLesson/Chapter_4_Tank/Tiger.java
@@ -0,0 +1,37 @@
+package ThirdLesson.Chapter_4_Tank;
+
+import java.awt.*;
+
+public class Tiger extends AbstractTank {
+    protected int armor;
+
+    public Tiger(int x, int y, Direction direction, ActionField actionField, BattleField battleField, int armor) {
+        super(x, y, direction, actionField, battleField);
+        this.armor = armor;
+    }
+
+    @Override
+    public void destroy() {
+        if (armor == 0) {
+            super.destroy();
+        }
+        armor--;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(new Color(255, 0, 255));
+        graphics.fillRect(this.getX(), this.getY(), 64, 64);
+
+        graphics.setColor(new Color(255, 255, 0));
+        if (this.getDirection() == Direction.UP) {
+            graphics.fillRect(this.getX() + 20, this.getY(), 24, 34);
+        } else if (this.getDirection() == Direction.DOWN) {
+            graphics.fillRect(this.getX() + 20, this.getY() + 30, 24, 34);
+        } else if (this.getDirection() == Direction.LEFT) {
+            graphics.fillRect(this.getX(), this.getY() + 20, 34, 24);
+        } else {
+            graphics.fillRect(this.getX() + 30, this.getY() + 20, 34, 24);
+        }
+    }
+}

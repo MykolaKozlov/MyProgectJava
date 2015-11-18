32cc2e5992502b3ad2f9479bec44b0fc01566e30
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-18 15:24:00 +0200

    bullet draw and destroy interfes

---
+++ src/ThirdLesson/Chapter_4_Tank/Bullet.java
@@ -0,0 +1,51 @@
+package ThirdLesson.Chapter_4_Tank;
+
+import java.awt.*;
+
+public class Bullet implements Destroyeble, Drowable{
+    private int x;
+    private int y;
+    private int speed = 5;
+    private Direction direction;
+
+    public Bullet(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void updateX(int x) {
+        this.x += x;
+    }
+
+    public void updateY(int y) {
+        this.y += y;
+    }
+
+    public void destroy() {
+        x = -100;
+        y = -100;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(new Color(255, 255, 0));
+        graphics.fillRect(this.getX(), this.getY(), 14, 14);
+    }
+}

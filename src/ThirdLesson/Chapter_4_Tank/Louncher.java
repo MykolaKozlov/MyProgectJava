083d9156ce3c07dba05f7cc5855c779b21db5f1b
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-18 15:26:12 +0200

    no change

---
+++ src/ThirdLesson/Chapter_4_Tank/Louncher.java
@@ -0,0 +1,8 @@
+package ThirdLesson.Chapter_4_Tank;
+
+public class Louncher {
+    public static void main(String[] args) throws Exception {
+        ActionField actionField = new ActionField();
+        actionField.runTheGame();
+    }
+}

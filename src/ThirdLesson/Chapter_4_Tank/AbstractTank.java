b516bcb510fbd8ed3f6531a0b5b0c0caf3f68e5b
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-18 15:17:56 +0200

    abstractTank draw and destroy interface

--- src/ThirdLesson/Chapter_4_Tank/AbstractTank.java
+++ src/ThirdLesson/Chapter_4_Tank/AbstractTank.java
@@ -1,6 +1,8 @@
 package ThirdLesson.Chapter_4_Tank;
 
-public abstract class AbstractTank {
+import java.awt.*;
+
+public abstract class AbstractTank implements Drowable, Destroyeble{
     protected int speed = 10;
     protected int x;
     protected int y;
@@ -170,5 +172,22 @@ public abstract class AbstractTank {
     public void destroy() {
         x = -100;
         y = -100;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(new Color(255, 0, 0));
+        graphics.fillRect(this.getX(), this.getY(), 64, 64);
+
+        graphics.setColor(new Color(0, 255, 0));
+        if (this.getDirection() == Direction.UP) {
+            graphics.fillRect(this.getX() + 20, this.getY(), 24, 34);
+        } else if (this.getDirection() == Direction.DOWN) {
+            graphics.fillRect(this.getX() + 20, this.getY() + 30, 24, 34);
+        } else if (this.getDirection() == Direction.LEFT) {
+            graphics.fillRect(this.getX(), this.getY() + 20, 34, 24);
+        } else {
+            graphics.fillRect(this.getX() + 30, this.getY() + 20, 34, 24);
+        }
     }
 }

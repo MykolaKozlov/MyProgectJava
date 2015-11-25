cf9084b09799ee94387dd705aef5d716c51c318a
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-25 14:49:22 +0200

    add list

---
+++ src/FourthLesson/Chapter_2_Task_1/SimpleLinkedList.java
@@ -0,0 +1,43 @@
+package FourthLesson.Chapter_2_Task_1;
+
+public class SimpleLinkedList extends Node {
+
+    private Node root;
+    private int size;
+
+    public SimpleLinkedList() {
+
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void addFirst(Object object) {
+
+    }
+
+    public void addLast(Object object) {
+
+    }
+
+    public void addAfter(Object object, Object objectAfter) {
+
+    }
+
+    public int getSise() {
+        return getSize();
+    }
+}

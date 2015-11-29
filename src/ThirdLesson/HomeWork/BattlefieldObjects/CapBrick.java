b051f8805faadb8414d47d4494e5ab86c33be7a2
MykolaKozlov <mykolakozlov@gmail.com>
2015-11-29 14:37:07 +0200

    add CapBrick

---
+++ src/ThirdLesson/HomeWork/BattlefieldObjects/CapBrick.java
@@ -0,0 +1,12 @@
+package ThirdLesson.HomeWork.BattlefieldObjects;
+
+public class CapBrick extends AbstractBattleFieldObject {
+
+    public CapBrick() {
+
+    }
+
+    public CapBrick(int x, int y) {
+        super(x, y);
+    }
+}

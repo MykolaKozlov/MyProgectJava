ab202fbc4bf32984d1566bab951bf2a2a386d261
MykolaKozlov <mykolakozlov@gmail.com>
2015-12-01 00:05:35 +0200

    add lastElement in list change

--- src/FourthLesson/Chapter_2_Task_7/Demo.java
+++ src/FourthLesson/Chapter_2_Task_7/Demo.java
@@ -4,6 +4,13 @@ public class Demo {
     public static void main(String[] args) {
         SimpleLinkedList simpleLinkedList = new SimpleLinkedList();
         simpleLinkedList.addFirst("1");
+        simpleLinkedList.addLast("100");
+        simpleLinkedList.addLast("250");
+        simpleLinkedList.addFirst("100");
+//        simpleLinkedList.addLast("200");
+
+//        simpleLinkedList.addLast("5");
+
 //        simpleLinkedList.addFirst("2");
 //        simpleLinkedList.addFirst("3");
         simpleLinkedList.printList();
--- src/FourthLesson/Chapter_2_Task_7/SimpleLinkedList.java
+++ src/FourthLesson/Chapter_2_Task_7/SimpleLinkedList.java
@@ -25,8 +25,20 @@ public class SimpleLinkedList {
     }
 
     public void addLast(Object object) {
-
-
+        Node element = new Node();
+        element.object = object;
+        if (firstElementOfList == null) {
+            firstElementOfList = element;
+        } else {
+            Node first = firstElementOfList;
+            Node last = firstElementOfList;
+            while (first.refNestElement != null) {
+                first = first.refNestElement;
+                last = first;
+            }
+            last.refNestElement = element;
+        }
+        size++;
     }
 
     public void addAfter(Object object, Object objectAfter) {
@@ -40,8 +52,12 @@ public class SimpleLinkedList {
     public void printList() {
         Node element = firstElementOfList;
         while (size != 0) {
-            System.out.println(element.object + " ");
-            element = element.refNestElement;
+            if (element == null) {
+                System.out.println("null");
+            } else {
+                System.out.println(element.object + " ");
+                element = element.refNestElement;
+            }
             size--;
         }
     }

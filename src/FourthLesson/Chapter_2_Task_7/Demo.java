af2d2fb4fde2d1af603f0b4b77a3564c32c315df
MykolaKozlov <mykolakozlov@gmail.com>
2015-12-02 00:10:28 +0200

    add lastElement

--- src/FourthLesson/Chapter_2_Task_7/Demo.java
+++ src/FourthLesson/Chapter_2_Task_7/Demo.java
@@ -3,18 +3,14 @@ package FourthLesson.Chapter_2_Task_7;
 public class Demo {
     public static void main(String[] args) {
         SimpleLinkedList simpleLinkedList = new SimpleLinkedList();
+        simpleLinkedList.addFirst("-120");
+        simpleLinkedList.addLast("1");
+        simpleLinkedList.addLast("5");
+        simpleLinkedList.addLast("10");
+        simpleLinkedList.addLast("10");
+        simpleLinkedList.addLast("20");
         simpleLinkedList.addFirst("1");
-        simpleLinkedList.addLast("100");
-        simpleLinkedList.addLast("250");
-        simpleLinkedList.addFirst("100");
-//        simpleLinkedList.addLast("200");
-
-//        simpleLinkedList.addLast("5");
-
-//        simpleLinkedList.addFirst("2");
-//        simpleLinkedList.addFirst("3");
         simpleLinkedList.printList();
-//        System.out.println(simpleLinkedList.getSise());
-
+        System.out.println("List size: " + simpleLinkedList.getSize());
     }
 }
--- src/FourthLesson/Chapter_2_Task_7/SimpleLinkedList.java
+++ src/FourthLesson/Chapter_2_Task_7/SimpleLinkedList.java
@@ -2,63 +2,55 @@ package FourthLesson.Chapter_2_Task_7;
 
 public class SimpleLinkedList {
 
-    private class Node {
-        Object object;
-        Node refNestElement;
-    }
-
-    private Node firstElementOfList;
+    private Node firstElementInList;
+    private Node lastElementInList;
     private int size;
 
     public SimpleLinkedList() {
+        firstElementInList = null;
         size = 0;
     }
 
+    public class Node {
+        public Object object;
+        public Node refNexrElementInList;
+    }
+
     public void addFirst(Object object) {
-        Node element = new Node();
-        element.object = object;
-        if (firstElementOfList != null) {
-            element.refNestElement = firstElementOfList;
+        Node node = new Node();
+        node.object = object;
+        if (firstElementInList == null) {
+            firstElementInList = node;
+            lastElementInList = node;
+        } else {
+            node.refNexrElementInList = firstElementInList;
+            firstElementInList = node;
         }
-        firstElementOfList = element;
         size++;
     }
 
     public void addLast(Object object) {
-        Node element = new Node();
-        element.object = object;
-        if (firstElementOfList == null) {
-            firstElementOfList = element;
+        Node node = new Node();
+        node.object = object;
+        if (firstElementInList == null) {
+            firstElementInList = node;
+            lastElementInList = node;
         } else {
-            Node first = firstElementOfList;
-            Node last = firstElementOfList;
-            while (first.refNestElement != null) {
-                first = first.refNestElement;
-                last = first;
-            }
-            last.refNestElement = element;
+            lastElementInList.refNexrElementInList = node;
+            lastElementInList = node;
         }
         size++;
     }
 
-    public void addAfter(Object object, Object objectAfter) {
-
-    }
-
-    public int getSise() {
+    public int getSize() {
         return size;
     }
 
     public void printList() {
-        Node element = firstElementOfList;
-        while (size != 0) {
-            if (element == null) {
-                System.out.println("null");
-            } else {
-                System.out.println(element.object + " ");
-                element = element.refNestElement;
-            }
-            size--;
+        Node node = firstElementInList;
+        while (node != null) {
+            System.out.println(node.object + " ");
+            node = node.refNexrElementInList;
         }
     }
 }

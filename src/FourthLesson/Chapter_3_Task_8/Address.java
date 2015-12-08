f2b380e2899a03ea785361f3818937ec965cbad5
MykolaKozlov <mykolakozlov@gmail.com>
2015-12-08 22:22:18 +0200

    Address hashCod and equals

---
+++ src/FourthLesson/Chapter_3_Task_8/Address.java
@@ -0,0 +1,35 @@
+package FourthLesson.Chapter_3_Task_8;
+
+public class Address {
+    private String city;
+    private String street;
+    private int house;
+
+    public Address() {
+
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+}

6aff3eaea27440d87a42cef1c99ea00bc1b6d595
MykolaKozlov <mykolakozlov@gmail.com>
2015-12-08 22:02:52 +0200

    hashCod and equals

---
+++ src/FourthLesson/Chapter_3_Task_6/Person.java
@@ -0,0 +1,56 @@
+package FourthLesson.Chapter_3_Task_6;
+
+public class Person {
+    private String name;
+    private int age;
+    private long salary;
+
+    public Person(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Person person = (Person) object;
+
+        if (age != person.age) return false;
+        if (salary != person.salary) return false;
+        return name.equals(person.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + (int) (salary ^ (salary >>> 32));
+        return result;
+    }
+}

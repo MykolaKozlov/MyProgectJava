c0f956b254d53cc6233cb4320e50d3c93f94e0c4
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-04 19:11:49 +0200

    add Window colour

---
+++ src/FiveLesson/Chapter_1_Task_8/Demo.java
@@ -0,0 +1,8 @@
+package FiveLesson.Chapter_1_Task_8;
+
+public class Demo {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.drow();
+    }
+}
---
+++ src/FiveLesson/Chapter_1_Task_8/Window.java
@@ -0,0 +1,23 @@
+package FiveLesson.Chapter_1_Task_8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(300, 100, 800, 600);
+
+    }
+
+    public void drow() {
+        JFrame jFrame = new JFrame("Window");
+        jFrame.setBounds(300, 100, 800, 600);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+//        jFrame.setBackground(Color.BLUE);
+        jFrame.getContentPane().add(this);
+        jFrame.setVisible(true);
+    }
+}

7f8558a86ce6300c89ed5a1e5b3a5e9dec151784
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-04 15:25:59 +0200

    add Window

---
+++ src/FiveLesson/Chapter_1_Task_4/Window.java
@@ -0,0 +1,11 @@
+package FiveLesson.Chapter_1_Task_4;
+
+import javax.swing.*;
+
+public class Window {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("MyFirstWindow");
+        jFrame.setBounds(300, 100, 800, 600);
+        jFrame.setVisible(true);
+    }
+}

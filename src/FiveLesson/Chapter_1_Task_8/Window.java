15cb1deda5994f43290fb055e3a52ed1d92eafed
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-05 20:29:40 +0200

    window change2

--- src/FiveLesson/Chapter_1_Task_8/Window.java
+++ src/FiveLesson/Chapter_1_Task_8/Window.java
@@ -7,14 +7,16 @@ public class Window extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
+//        g.setColor(Color.BLUE);
+//        g.fillRect(0, 0, 800, 600);
     }
 
     public void drow() {
         JFrame jFrame = new JFrame("Window");
+        jFrame.getContentPane().add(this);
+        jFrame.setBackground(Color.BLUE);
         jFrame.setBounds(300, 100, 800, 600);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
-        jFrame.setBackground(Color.BLUE);
-        jFrame.getContentPane().add(this);
         jFrame.setVisible(true);
     }
 }

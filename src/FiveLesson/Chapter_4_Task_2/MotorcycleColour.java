177ef98702c6291863c2f583ad5724aa07cdbadc
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-08 21:49:30 +0200

    add window shop

---
+++ src/FiveLesson/Chapter_4_Task_2/MotorcycleColour.java
@@ -0,0 +1,15 @@
+package FiveLesson.Chapter_4_Task_2;
+
+public enum MotorcycleColour {
+    RED(0), ORANGE(1), GREEN(2), BLUE(3), YELLOW(4), BLACK(5), WHITE(6), GREY(7);
+
+    private int id;
+
+    private MotorcycleColour(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}

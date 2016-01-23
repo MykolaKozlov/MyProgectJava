016a37e47941443c0726c437076f6d9cf34c15dc
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-23 20:35:17 +0200

    change adapter

--- src/SixLesson/Chapter_1_Task_3/v2/Launcher.java
+++ src/SixLesson/Chapter_1_Task_3/v2/Launcher.java
@@ -1,14 +1,14 @@
 package SixLesson.Chapter_1_Task_3.v2;
 
+import SixLesson.Chapter_1_Task_3.v2.Calc.ArifmeticaCalculator;
 import SixLesson.Chapter_1_Task_3.v2.Calc.Calculator;
-import SixLesson.Chapter_1_Task_3.v2.Calc.DefaultCalculator;
 
 import java.util.Random;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		Calculator calc = new DefaultCalculator();
+		Calculator calc = new ArifmeticaCalculator();
 		
 		Operations o = new Operations();
 		o.setCalc(calc);

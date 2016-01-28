2c04e23c5b341f59a439d4dc51b2a652596a5d01
MykolaKozlov <mykolakozlov@gmail.com>
2016-01-28 23:30:25 +0200

    create splash screen

---
+++ src/SixLesson/Chapter_4_Task_3/DemoUI.java
@@ -0,0 +1,8 @@
+package SixLesson.Chapter_4_Task_3;
+
+public class DemoUI {
+    public static void main(String[] args) {
+        MotoShopUI motoShopUI = new MotoShopUI();
+        motoShopUI.motoShopPanel();
+    }
+}
---
+++ src/SixLesson/Chapter_4_Task_3/MotorcycleBrand.java
@@ -0,0 +1,15 @@
+package SixLesson.Chapter_4_Task_3;
+
+public enum MotorcycleBrand {
+    HONDA(0), BMW(1), DUCATI(2), HARLEY_DAVIDSON(3), JAVA(4), KAWASAKI(5), KTM(6), SUZUKI(7), YAMAHA(8);
+
+    private int id;
+
+    private MotorcycleBrand(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}

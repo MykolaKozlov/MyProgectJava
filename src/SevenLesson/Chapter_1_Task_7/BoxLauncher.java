c1076197a1ab6dc91986086ecdd809817720ef46
MykolaKozlov <mykolakozlov@gmail.com>
2016-02-07 15:48:56 +0200

    add sort in my box change

--- src/SevenLesson/Chapter_1_Task_7/BoxLauncher.java
+++ src/SevenLesson/Chapter_1_Task_7/BoxLauncher.java
@@ -10,13 +10,13 @@ public class BoxLauncher {
         BMW bmw = new BMW();
         bmw.setBrand("BMW");
         bmw.setModel("S 1000 RR");
-        bmw.setMotorcycleColour(MotorcycleColour.BLACK);
+        bmw.setMotorcycleColour(MotorcycleColour.WHITE);
         bmw.setPrice(10000);
 
         BMW bmw2 = new BMW();
         bmw2.setBrand("BMW");
         bmw2.setModel("K 1600 GT");
-        bmw2.setMotorcycleColour(MotorcycleColour.WHITE);
+        bmw2.setMotorcycleColour(MotorcycleColour.ORANGE);
         bmw2.setPrice(5000);
 
 
@@ -25,10 +25,8 @@ public class BoxLauncher {
         motorcycleBox.add(bmw);
         motorcycleBox.add(bmw2);
 
-//        motorcycleBox.sortByPrice();
+        motorcycleBox.sortByPrice();
 
         motorcycleBox.printBox();
-
-
     }
 }

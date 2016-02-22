722495683a343aed05a043ac66ef6912bd274f2d
MykolaKozlov <mykolakozlov@gmail.com>
2016-02-22 23:24:07 +0200

    int to byte

---
+++ src/EightLesson/Chapter_1_Task_/IntToByte.java
@@ -0,0 +1,11 @@
+package EightLesson.Chapter_1_Task_;
+
+public class IntToByte {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 300; i++) {
+            System.out.println("int = " + " " + i);
+            System.out.println("byte = " + " " + (byte)i);
+        }
+    }
+}

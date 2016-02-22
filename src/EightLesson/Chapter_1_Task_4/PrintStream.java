d25a38c8462b2ebf7110caa621f150b6096fee6c
MykolaKozlov <mykolakozlov@gmail.com>
2016-02-22 23:52:38 +0200

    print stream change

--- src/EightLesson/Chapter_1_Task_4/PrintStream.java
+++ src/EightLesson/Chapter_1_Task_4/PrintStream.java
@@ -6,10 +6,10 @@ public class PrintStream {
     public static void main(String[] args) {
         byte[] bytes = {10, 20, -12, 121, 18, -128, 11, 10, 1};
         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
-        printSteramData(stream);
+        printStreamData(stream);
     }
 
-    public static void printSteramData(ByteArrayInputStream stream) {
+    public static void printStreamData(ByteArrayInputStream stream) {
         int i;
         while ((i = stream.read()) != -1) {
             System.out.println((byte)i);
